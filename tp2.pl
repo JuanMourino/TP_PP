9577366fd876688c700d30ca305b54e4c284e9d7
Tomiitooo <tomyrn23@gmail.com>
2024-06-22 21:12:45 -0300

    funcion vecinos chequea que la posicion pasada es valida

--- tp2.pl
+++ tp2.pl
@@ -50,7 +50,7 @@ crearPosicion(pos(X, Y), pos(X1, Y1)) :- X1 is X, Y1 is Y+1.
 posicionValida(pos(0, Y), [Z | _]) :- Y >= 0, N is Y+1, fila(N, W), append(W, _, Z).
 posicionValida(pos(X, Y), [_ | Zs]) :- X > 0, N is X-1, posicionValida(pos(N, Y), Zs).
 
-vecino(pos(X, Y), T, pos(X1, Y1)) :- crearPosicion(pos(X, Y), pos(X1, Y1)), posicionValida(pos(X1, Y1), T).
+vecino(pos(X, Y), T, pos(X1, Y1)) :- posicionValida(pos(X, Y), T), crearPosicion(pos(X, Y), pos(X1, Y1)), posicionValida(pos(X1, Y1), T).
 
 %% Ejercicio 4
 %% vecinoLibre(+Pos, +Tablero, -PosVecino) idem vecino/3 pero además PosVecino
